4fb4d9109c98c8338125dae6b22a54a800242ff6
mattbrailsford <me@mattbrailsford.com>
2015-06-17 16:46:47 +0100

    Converted features partial to DitFlo

---
+++ Src/DitFlo.Web/App_Code/Models/FeatureLink.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using DitFlo.Ditto.ValueResolvers;
+using Our.Umbraco.Ditto;
+
+namespace DitFlo.Models
+{
+    public class FeatureLink : Link
+    {
+        public string Image { get; set; }
+
+        [DittoValueResolver(typeof(ExtractResolver))]
+        public HtmlString Extract { get; set; }
+    }
+}
